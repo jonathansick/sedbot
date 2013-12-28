3b3b21c465533f697dac3a0034ce60428cdcac28
Jonathan Sick <jonathansick@mac.com>
2013-12-27 23:40:49 -0700

    Add normal prior function factory
    Uses scipy.stats.normal.logpdf to generate probabilities. The factory
    bakes the mean and standard deviation into the prior function.

--- sedbot/probf.py
+++ sedbot/probf.py
@@ -5,6 +5,7 @@ Library of built-in probability functions.
 """
 
 import numpy as np
+import scipy.stats
 
 
 def ln_uniform_factory(lower, upper):
@@ -31,4 +32,27 @@ def ln_uniform_factory(lower, upper):
             return scipy.stats.uniform.logpdf(x, loc=lower, scale=width)
         else:
             return -np.inf
+    return func
+
+
+def ln_normal_factory(mu, sigma):
+    """Log of normal prior probability factory.
+
+    Parameters
+    ----------
+    lower : float
+        Lower bound of the log-normal probability distribution.
+    upper : float
+        Upper bound of the log-normal probability distribution.
+
+    Returns
+    -------
+    func : function
+        A function that accepts a random variable and returns the log-normal
+        probability of that value. That is, an RV within the bounds has a 
+        ln-probability of 0, and -infinity if outside the bounds.
+    """
+    def func(x):
+        """Log of normal prior probability factory."""
+        return scipy.stats.norm.logpdf(x, loc=mu, scale=sigma)
     return func
