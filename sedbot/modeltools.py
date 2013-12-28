35a8498473be81dfb176575461a1cea82e211a32
Jonathan Sick <jonathansick@mac.com>
2013-12-28 13:50:31 -0700

    Add EmceeTimer class to modeltools
    EmceeTimer provides a time facility for sampler.run(). It provides
    a useful string representation to print the elapsed time and time
    per likelihood call after an emcee run.

--- sedbot/modeltools.py
+++ sedbot/modeltools.py
@@ -4,6 +4,9 @@
 Utilities for setting up models.
 """
 
+import time
+
+
 def reset_seed_limits(start_points, lower, upper):
     """In-place replacement of starting point seed values that are above/below
     specified limits.
@@ -25,3 +28,72 @@ def reset_seed_limits(start_points, lower, upper):
     """
     start_points[start_points < lower] = lower
     start_points[start_points > upper] = upper
+
+
+class EmceeTimer(object):
+    """Timer for emcee runs. Computes total run time and mean time of each
+    likelihood call.
+
+    Example::
+
+    >>> with EmceeTimer(n_steps, n_walkers) as emceet:
+    >>>     sampler.run_mcmc(p0, n_steps)
+    >>> print emceet
+
+    Parameters
+    ----------
+    nsteps : int
+        Number of emcee steps.
+    nwalkers : int
+        Number of emcee walkers.
+    """
+    def __init__(self, nsteps, nwalkers):
+        super(EmceeTimer, self).__init__()
+        self._nsteps = nsteps
+        self._nwalkers = nwalkers
+        self._start = None
+        self._stop = None
+        self._endtime = None
+        self._interval = None
+
+    def __enter__(self):
+        self._start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self._end = time.clock()
+        self._endtime = time.localtime()
+        self._interval = self._end - self._start
+
+    def __str__(self):
+        dt, unit = self._human_time(self._interval)
+        ct, cunit = self._human_time(self.seconds_per_call)
+        enddate = time.strftime('%Y-%m-%d %H:%M:%S', self._endtime)
+        l1 = "Finished emcee run at {enddate}"
+        l2 = "\tRun time: {dt} {unit}"
+        l3 = "\t{ct} {cunit} per call"
+        return "\n".join((l1, l2, l3)).format(dt=dt, unit=unit,
+                ct=ct, cunit=cunit, enddate=enddate)
+
+    def _human_time(self, interval):
+        """Return a time interval scaled to human-usable units."""
+        if interval < 60.:
+            dt, unit = interval, "seconds"
+        if interval >= 60.:
+            dt, unit = interval / 60., "minutes"
+        elif interval >= 3600.:
+            dt, unit = interval / 3600., "hours"
+        elif interval >= 86400:
+            dt, unit = interval / 86400., "days"
+        return dt, unit
+
+    @property
+    def seconds_per_call(self):
+        """The mean number of seconds elapsed per likelihood call.
+        
+        Returns
+        -------
+        interval : float
+            Description
+        """
+        return self._interval / float(self._nsteps * self._nwalkers)
