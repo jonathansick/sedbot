5bc3faafb0028a5a4e09321c30cd0b7cb09cec9b
Jonathan Sick <jonathansick@mac.com>
2013-12-29 00:33:19 -0700

    Create triangle plot function
    This function in sedbot.plots uses dfm's triangle.py module to generate
    corner plots that show 1D and 2D projections of posterior distributions
    and covariances between parameters.

--- sedbot/plots.py
+++ sedbot/plots.py
@@ -12,9 +12,7 @@ import matplotlib.gridspec as gridspec
 
 def chain_plot(path, sampler, param_names, limits):
     """docstring for chain_plot"""
-    plotdir = os.path.dirname(path)
-    if len(plotdir) > 0 and not os.path.exists(plotdir):
-        os.makedirs(plotdir)
+    _prep_plot_dir(path)
 
     nwalkers, nsteps, ndim = sampler.chain.shape
     steps = np.arange(nsteps)
@@ -25,13 +23,13 @@ def chain_plot(path, sampler, param_names, limits):
             bottom=0.05, top=0.99,
             wspace=None, hspace=0.1, width_ratios=None, height_ratios=None)
     axes = {}
-    for i, name in enumerate(param_names):
+    for i, (name, limit) in enumerate(zip(param_names, limits)):
         ax = fig.add_subplot(gs[i])
         for j in xrange(nwalkers):
             ax.plot(steps, sampler.chain[j, :, i], '-', lw=0.5, alpha=0.5)
         ax.set_xlim(steps.min(), steps.max())
         if limits is not None and name in limits:
-            ax.set_ylim(*limits[name])
+            ax.set_ylim(*limit)
         name_clean = name.replace("_", "\_")
         ax.set_ylabel(name_clean)
         axes[name] = ax
@@ -42,3 +40,28 @@ def chain_plot(path, sampler, param_names, limits):
 
     gs.tight_layout(fig, pad=1.08, h_pad=None, w_pad=None, rect=None)
     canvas.print_figure(path + ".pdf", format="pdf")
+
+
+def triangle_plot(path, samples, param_names, limits,
+        figsize=(5, 5), truths=None):
+    """Make a corner plot using the triangle.py package.
+    
+    Triangle/corner plots are convenient visualizations of posterior
+    distributions and covariances between parameters.
+
+    samples = sampler.chain[:, nburn:, :].reshape((-1, ndim))
+    """
+    from triangle import corner
+    _prep_plot_dir(path)
+    clean_labels = [l.replace("_", "\_") for l in param_names]
+    fig = corner(samples, labels=clean_labels, extents=limits, truths=truths,
+            truth_color="#4682b4", scale_hist=False, quantiles=[],
+            verbose=True, plot_contours=True, plot_datapoints=True,
+            fig=None)
+    fig.savefig(path)
+
+
+def _prep_plot_dir(path):
+    plotdir = os.path.dirname(path)
+    if len(plotdir) > 0 and not os.path.exists(plotdir):
+        os.makedirs(plotdir)
