cbb77768ca6265706e16323dc594d89b9f2c54d6
Jonathan Sick <jonathansick@mac.com>
2014-10-01 23:53:32 -0400

    Hook up a mock dataset generator

--- scripts/multipix_demo.py
+++ scripts/multipix_demo.py
@@ -5,18 +5,19 @@ Demonstration for the multipix package.
 """
 
 import numpy as np
+from fsps import StellarPopulation
 
 from sedbot.multipix.models import ThreeParamSFH
 from sedbot.multipix.sampler import MultiPixelGibbsBgSampler
 
+from sedbot.modeltools import mock_dataset
+
 
 def main():
     npix = 2
-    sed_bands = "ugri"
-    nbands = len(sed_bands)
-    seds = np.ones((npix, nbands))
-    sed_errs = np.ones((npix, nbands))
-    sed_bands = None
+    sed_bands = ['sdss_u', 'sdss_g', 'sdss_r', 'sdss_i']
+    seds, sed_errs = make_dataset(npix, sed_bands)
+    print seds
 
     def model_initializer():
         m = ThreeParamSFH(seds, sed_errs, sed_bands)
@@ -24,6 +25,37 @@ def main():
 
     sampler = MultiPixelGibbsBgSampler(model_initializer)
     print sampler
+    print sampler._model
+
+
+def make_dataset(npix, bands):
+    PSET_INIT = {'add_dust_emission': True,
+                 'add_stellar_remnants': True,
+                 'compute_vega_mags': False,  # use AB mags
+                 'dust_type': 0,  # Power law dust
+                 'sf_start': 3.,
+                 'sfh': 1,
+                 'tburst': 10.,
+                 'fburst': 0.,
+                 'dust1': 0.2,
+                 'dust2': 0.2,
+                 'zmet': 20}
+    sp = StellarPopulation(**PSET_INIT)
+
+    nbands = len(bands)
+    seds = np.empty((npix, nbands), dtype=np.float)
+    sed_errs = np.empty((npix, nbands), dtype=np.float)
+    for i in xrange(npix):
+        pix_sed, pix_sigma = mock_dataset(sp,
+                                          bands,
+                                          785 * 1000.,
+                                          1e8,
+                                          0.0,
+                                          0.05,
+                                          apply_errors=True)
+        seds[i, :] = pix_sed.T
+        sed_errs[i, :] = pix_sigma.T
+    return seds, sed_errs
 
 
 if __name__ == '__main__':
