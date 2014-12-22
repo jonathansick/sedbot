027d7f0748d5543dab2a2b61cfe425c5d30701d7
Jonathan Sick <jonathansick@mac.com>
2014-12-22 07:16:37 -0500

    Add dn_to_micro_jy function

--- sedbot/photconv.py
+++ sedbot/photconv.py
@@ -29,6 +29,39 @@ def _flux_err_to_mag_err(flux_sigma, flux):
     return 1.0875 * flux_sigma / flux
 
 
+def dn_to_micro_jy(dn, zp, err=None):
+    """Convert pixel values (DN) to µJy.
+
+    Parameters
+    ----------
+    dn : array
+        Pixel values.
+    zp : array or scalar
+        Zeropoint (or zeropoints) corresponding to the DN measurements
+        to bring them into the AB system, where
+
+        .. math::
+           m_\mathrm{AB} = -2.5 \log_{10} (\mathrm{DN}) + \mathrm{zp}
+    err : array
+        Uncertainties in DN (optional), interpreted as 1-sigma
+        Gaussian errors.
+
+    Returns
+    -------
+    mjy : array
+        Fluxes, as micro-janskies
+    mjy_err : array
+        (If errors are provided) the 1-sigma uncertainties in flux.
+    """
+    f = 10. ** (-0.4 * (zp + 48.6) + 6. + 23.)
+    mjy = dn * f
+    if err is not None:
+        mjy_err = err * f
+        return mjy, mjy_err
+    else:
+        return mjy
+
+
 def ab_mag_to_micro_jy(mags, err=None):
     r"""Convert scalar (or array) AB magnitudes to µJy.
 
